9393b9c7fa332e7a6acafd4cb0558e507ad31ecc
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-11-30 17:15:47 -0800

    BOOLEAN LOGIC SUBMISSION ASSIGNMENT

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    /*
+    static void Main()
+    {
+        int packageWidth = 10;
+        int packageHeight = 25;
+        //BOOLEAN LOGIC (&&)
+        bool tooBigAnd = (packageHeight > 25 && packageWidth > 25);
+        Console.WriteLine("Are both the package height and the package width greater than 25?" + tooBigAnd);
+        Console.ReadLine();
+
+        //BOOLEAN LOGIC (||)
+        bool tooBigOr = (packageHeight > 25 || packageWidth > 25);
+        Console.WriteLine("Are either the package height or the package width greater than 25?" + tooBigOr);
+        Console.ReadLine();
+
+        String firstName = "Jesse";
+        String lastNAme = "Jonhson";
+        //BOOLEAN LOGIC MULTIPLE USES OF OR
+        String socialSecurutyNumber = "111-11-1111";
+        bool isAuthorized = (firstName == "Jesse" && lastNAme == "Jonhson" && socialSecurutyNumber == "111-11-1111");
+        Console.WriteLine("Is this person’s first name ‘Jesse’ and his last name ‘Johnson’ and is his social security number ‘111-11-1111’?" + isAuthorized);
+        Console.ReadLine();
+
+        int bankAccountBalance = 500;
+        bool isInRockBand = false;
+        string name = "Jesse";
+        bool isReallyCool = (bankAccountBalance > 500 || isInRockBand || name == "Jesse");
+        Console.WriteLine("Does this person have over a hundred thousand dollars in his bank account or is he in a rock band or is his name Jesse?" + isReallyCool);
+        
+
+        //BOOLEAN LOGIC COMBINING && AND ||
+        bool result_a = (true && (true || false));
+        Console.WriteLine(result_a);
+
+        //BOOLEAN LOGIC COMBINING && AND
+        Console.WriteLine("false && false: "+(false && false));
+        Console.WriteLine("false && true: " + (false && true));
+        Console.WriteLine("true && true: " + (true && true));
+        Console.ReadLine();
+
+        //BOOLEAN LOGIC COMBINING || OR
+        Console.WriteLine("false || false: " + (false || false));
+        Console.WriteLine("false || true: " + (false || true));
+        Console.WriteLine("true || true: " + (true || true));
+        Console.ReadLine();
+
+        //BOOLEAN LOGIC COMBINING != 
+        Console.WriteLine("false != false: " + (false != false));
+        Console.WriteLine("false != true: " + (false != true));
+        Console.WriteLine("true != true: " + (true != true));
+
+        //BOOLEAN LOGIC COMBINING  ^
+        Console.WriteLine("false ^ false: " + (false ^ false));
+        Console.WriteLine("false ^ true: " + (false ^ true));
+        Console.WriteLine("true ^ true: " + (true ^ true));
+        Console.ReadLine();
+        
+    }
+    */
+}
