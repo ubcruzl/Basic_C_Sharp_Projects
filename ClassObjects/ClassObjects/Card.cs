f4a8eea04b1ca6347a03245f5407fe378ecf726d
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-03 12:50:45 -0800

    CALLING METHODS SUBMISSION ASSIGNMENT

---
+++ ClassObjects/ClassObjects/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Encapsulation, is to make sure that "sensitive" data is hidden from users.
+
+namespace ClassObjects
+{             
+
+    public class Card                              //declarar campos / variables como private
+    {                                       
+        public Card()           //constructor
+        {                                       //A constructor is a special method that is used to initialize objects. The advantage of a constructor,
+                                                //is that it is called when an object of a class is created.
+                                                //It can be used to set initial values for fields:
+            Suit = "Spades";
+            Face = "Two";
+        }
+                                               //setters make your object mutable,
+        public string Suit { get; set; }    //you are creating a Property. 
+        public string Face { get; set; }       //The get method returns the value of the variable name./The set method assigns a value to the name variable
+    }
+
+}
