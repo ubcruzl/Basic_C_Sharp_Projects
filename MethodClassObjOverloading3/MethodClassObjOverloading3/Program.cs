70772796ad23b00d9e16663423da5e7fd09042cf
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-06 14:32:28 -0800

    METHOD CLASS SUBMISSION ASSIGNMENT

---
+++ MethodClassObjOverloading3/MethodClassObjOverloading3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassObjOverloading3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //the Main() method of the console app, instantiate the class.
+
+            Console.WriteLine("\ntest method 3, specifying the parameters by name : ");
+            Numeros.methodThree(numberOne: 5, numberTwo: 3);         //Call the method in the class, specifying the parameters by name.
+
+            Console.WriteLine("\ntest method 3, specifying the parameters by name, diferent order : ");
+            Numeros.methodThree(numberTwo: 3, numberOne: 5);        //Call the method in the class, specifying the parameters by name.
+
+            Console.ReadLine();
+        }
+    }
+}
