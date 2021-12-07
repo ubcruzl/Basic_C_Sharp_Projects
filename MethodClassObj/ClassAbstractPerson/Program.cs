15364c7a47c1698bce9b541961357a9e6a0a430d
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-07 01:28:48 -0800

    ABSTRACT CLASS SUBMISSION ASSIGNMENT

---
+++ MethodClassObj/ClassAbstractPerson/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAbstractPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate an Employee object with firstName “Sample” and lastName “Student”. 
+            Employee emply = new Employee();
+            emply.firstName = "Sample";
+            emply.lastName = "Student";
+            //Call the SayName() method on the object.
+            emply.SayName();
+            Console.ReadLine();
+        }
+    }
+}
