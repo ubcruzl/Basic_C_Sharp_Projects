ccb2bbba0ec4222fed436da9fd34940646c68b39
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-06 16:23:19 -0800

    CLASS SUBMISSION ASSIGNMENT

---
+++ MethodClassObjOverloading4/MethodClassObjOverloading4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassObjOverloading4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOut;
+
+            //In the Main() method, instantiate that class.
+            Numeros nums = new Numeros();
+            Console.WriteLine("test method 4: ");
+            Console.WriteLine("\nInput number to divide by 2 : ");
+            //
+            int number =Convert.ToInt32(Console.ReadLine());     //Have the user enter a number.
+            nums.methodDivided(number, out numberOut);     // Call the method on that number. 
+            Console.WriteLine("Result : "+ numberOut);  //Display the output to the screen. It should be the entered number, divided by two.
+
+            //Create a method with output parameters.
+            //accessing the method
+            nums.methodOut(out int x);
+            Console.WriteLine("accessing the method output parameters: " + x);
+
+            //accessing the method Overload a method.
+            Console.WriteLine("accessing the Overload   "+ nums.methodOverloadSum(5,5));
+            Console.WriteLine("accessing the Overload   " + nums.methodOverloadSum(5, 5.55f));
+
+            //accessing the method static method
+
+            Console.ReadLine();
+            Console.WriteLine("accessing the static method   " + Numeros.methodStaticMult(5,5));
+        }
+
+    }
+}
