936f0ba616de336866ea397f5bcbee2dde0b6257
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-06 22:27:22 -0800

    METHODS AND OBJECTS SUBMISSION ASSIGNMENT

---
+++ MethodClassObj/ClassPerson/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPerson
+{
+    // class called Employee and have it inherit from the Person class.
+    class Employee:Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ MethodClassObj/ClassPerson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ub = new Person();
+            ub.FirstName = "ub";
+            ub.LastName = "cr";
+            ub.SayName();
+            // instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+            Employee emply = new Employee();
+            emply.FirstName = "Sample";
+            emply.LastName = "Student";
+            //Call the superclass method SayName() on the Employee object.
+            emply.SayName();
+            Console.ReadLine();
+        }
+    }
+}
