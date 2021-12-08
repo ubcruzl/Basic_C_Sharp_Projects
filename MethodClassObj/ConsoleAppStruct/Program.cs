d0cf7a0562a3da31b0aeb59a596420cb3453bf5f
ubcruzl <40645692+ubcruzl@users.noreply.github.com>
2021-12-07 23:59:33 -0800

    STRUCT ASSIGNMENT

---
+++ MethodClassObj/ConsoleAppStruct/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //give it the property “Amount” and have it be of data type decimal.
+            Number number = new Number();
+            number.Amount = 12.4334m;
+            number.Amount = 12.4334m;
+            //Print this amount to the console.
+            Console.WriteLine("Struct Amount "+ number.Amount);
+            Console.ReadLine();
+        }
+    }
+    //Create a struct called Number
+
+    public struct Number
+    {
+        //give it the property “Amount” and have it be of data type decimal.
+        public decimal Amount;
+    }
+}
